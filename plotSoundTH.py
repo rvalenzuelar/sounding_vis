b8ab0010c3f60373972623a3c8f164ed90cd0c20
rvalenzuelar <raul.valenzuela@colorado.edu>
2015-10-16 17:26:08 -0600

    added more QC for interpolation, gaussian filter, and contour analysis of bvf

--- plotSoundTH.py
+++ plotSoundTH.py
@@ -25,6 +25,10 @@ from scipy.interpolate import interp1d
 
 from  scipy.interpolate import Rbf
 
+from scipy.ndimage.filters import gaussian_filter
+
+from scipy.spatial import cKDTree
+from itertools import product
 
 # import seaborn as sns
 
@@ -41,34 +45,85 @@ from  scipy.interpolate import Rbf
 def main():
 	
 	file_sound = get_sounding_files(None)
+	bvf_clevels=np.arange(-4e-4,10e-4,2e-4)
 
-	''' raw soundings '''
-	soundarray,_,_,_,_ = get_raw_array('thetaeq', file_sound)	
-	fig,ax=plt.subplots()
-	ax.imshow(soundarray,interpolation='none',origin='top',aspect='auto')
-	ax.invert_xaxis()
-	plt.subplots_adjust(left=0.125,right=0.9)	
-	plt.draw()	
+	''' raw soundings vertically-interpolated '''
+	# # soundarray,_,_,_,_ = get_raw_array('thetaeq', file_sound)	
+	# soundarray,_,_,_,_ = get_raw_array('bvf_moist', file_sound)	
+	# fig,ax=plt.subplots()
+	# # im=ax.imshow(soundarray,interpolation='none',origin='top',aspect='auto',vmin=298,vmax=308)
+	# im=ax.imshow(soundarray,interpolation='none',origin='top',aspect='auto',vmin=-8e-4,vmax=8e-4)
+	# ax.invert_xaxis()
+	# plt.subplots_adjust(left=0.125,right=0.9)	
+	# plt.colorbar(im)
+	# plt.draw()	
 
-	''' interpolated '''
-	soundarray2,_,_ = get_interp_array('thetaeq',files=file_sound)	
+	''' time-interpolated '''
+	# soundarray2,_,_ = get_interp_array('u',files=file_sound)	
+	# soundarray2,_,_ = get_interp_array('v',files=file_sound)	
+	# soundarray2,_,_ = get_interp_array('DD',files=file_sound)	
+	# soundarray2,_,_ = get_interp_array('thetaeq',files=file_sound)	
+	soundarray2,hgt,_ = get_interp_array('bvf_moist',files=file_sound)	
 
 	''' image plot '''
 	# fig,ax=plt.subplots()
-	# ax.imshow(soundarray2,interpolation='none',origin='top',aspect='auto')
+	# im=ax.imshow(soundarray2,interpolation='none',origin='top',aspect='auto',vmin=-8e-4,vmax=8e-4)
 	# ax.invert_xaxis()
 	# plt.subplots_adjust(left=0.125,right=0.9)	
+	# plt.colorbar(im)
 	# plt.draw()	
 
 	''' contourf plot '''
 	rows,cols = soundarray2.shape
 	X,Y = np.meshgrid(range(cols),range(rows))
 	fig,ax=plt.subplots()
-	ax.contourf(X,Y,soundarray2)
+	cf=ax.contourf(X,Y,soundarray2, levels=bvf_clevels)
+	# cf=ax.contourf(X,Y,soundarray2,levels=range(290,309))
+	plt.colorbar(cf)
 	ax.invert_xaxis()
 	plt.draw()	
 
+	''' smooth field
+	   sigma=3 seems good for BVF
+	 '''
+	sigma=2
+	sound_filtered = gaussian_filter(soundarray2, sigma,mode='nearest')
 
+	''' filtered image plot '''
+	# fig,ax=plt.subplots()
+	# # im=ax.imshow(sound_filtered,interpolation='none',origin='top',aspect='auto')
+	# im=ax.imshow(sound_filtered[10:,:],interpolation='none',origin='top',aspect='auto',vmin=-8e-4,vmax=8e-4)
+	# ax.invert_xaxis()
+	# plt.subplots_adjust(left=0.125,right=0.9)	
+	# plt.suptitle('smoothed with gaussian filter')
+	# plt.colorbar(im)	
+	# plt.draw()	
+
+	''' filtered contourf plot '''
+	rows,cols = soundarray2.shape
+	X,Y = np.meshgrid(range(cols),range(rows))
+	fig,ax=plt.subplots()
+	cf=ax.contourf(X,Y,sound_filtered,levels=bvf_clevels)
+	# cf=ax.contourf(X,Y,sound_filtered,levels=range(290,309))
+	plt.colorbar(cf)
+	ax.invert_xaxis()
+	plt.suptitle('smoothed with gaussian filter')
+	plt.draw()	
+
+	print np.sum(sound_filtered[10:60,:])
+	print hgt[10:60]
+
+	fig,ax=plt.subplots()
+	# im=ax.imshow(sound_filtered,interpolation='none',origin='top',aspect='auto')
+	im=ax.imshow(sound_filtered[0:60,:],interpolation='none',origin='top',aspect='auto',vmin=-8e-4,vmax=8e-4)
+	ax.invert_xaxis()
+	plt.subplots_adjust(left=0.125,right=0.9)	
+	plt.suptitle('smoothed with gaussian filter')
+	plt.colorbar(im)	
+	plt.draw()	
+
+
+	# plt.show()
 	plt.show(block=False)
 
 
@@ -92,19 +147,18 @@ def get_sounding_files(usr_case):
 
 def get_raw_array(soundvar,file_sound):
 
-	''' height grid '''
-	hgtgrid = np.asarray(range(12, 4000,12))
+	file_sound.sort()
 
-	''' time grid '''
-	dates=np.arange('2001-01-23 16:00', '2001-01-24 04:40', dtype='datetime64[20m]')
+	''' height grid with 10 m resolution'''
 
 	var=[] # 2D list
 	timestamps=[]
-	top_limit=4100 #[m]
-	file_sound.sort()
+	top_limit=10000 #[m]
+	hgtgrid = np.asarray(range(12, 4000,20))
 	for f in file_sound:
 		
-		''' timestamps '''
+		''' sounding timestamps approached to 
+		the nearest 20 minute grid point'''
 		fname = os.path.basename(f)
 		raw_date=fname[:-4].split('_')[-2:]
 		y=raw_date[0][:4]
@@ -118,6 +172,9 @@ def get_raw_array(soundvar,file_sound):
 			mm = 0
 		else:
 			mm = nearest
+		if hh == 24:
+			d=str(int(d)+1)
+			hh=0
 		raw_date=y+'-'+m+'-'+d+'T'+str(hh).zfill(2)+':'+str(mm).zfill(2)+':'+str(0).zfill(2)
 		timestamps.append(np.datetime64(raw_date))
 
@@ -125,13 +182,68 @@ def get_raw_array(soundvar,file_sound):
 		df = mf.parse_sounding(f)
 		sv= df[soundvar][ df.index<top_limit ].values
 		Z=df.index[ df.index<top_limit ].values
-
 		''' interpolate to a common vertical grid '''
 		f=interp1d(Z,sv)
-		svinterp=f(hgtgrid)
+		svinterp=np.asarray([])
+		for h in hgtgrid:
+			try:
+				svinterp=np.append(svinterp,f(h))
+			except ValueError:
+				svinterp=np.append(svinterp,np.nan)
 
-		''' add to list '''
-		var.append(svinterp)
+		''' take care of vertical nan values in raw soundings;
+		more gaps can show up if the top of the vertical grid 
+		is increased (i.e. higher balloon altitudes show more 
+		missing data)
+		'''
+		if np.any(np.isnan(svinterp)):
+			nanidx=np.where(np.isnan(svinterp))[0]
+			diff = np.diff(nanidx)			
+			idxjump = np.where(diff>1)[0]
+			ngaps= len(idxjump) +1
+			if ngaps>1 and nanidx[-1] !=svinterp.size-1 :
+				gapidx = np.split(nanidx,[idxjump+1])
+				for g in gapidx:
+					first=g[0]
+					last=g[-1]
+					''' pick good values between nans and 
+					make a linear interpolation (hopefully there
+					few nans) '''
+					if last+1 == svinterp.size:
+						x=[hgtgrid[first-1], hgtgrid[last]]
+						y=[svinterp[first-1], svinterp[last]]
+					elif first == 0:
+						x=[hgtgrid[first], hgtgrid[last+1]]
+						y=[svinterp[first], svinterp[last+1]]
+					else:
+						x=[hgtgrid[first-1], hgtgrid[last+1]]
+						y=[svinterp[first-1], svinterp[last+1]]
+					f=interp1d(x,y)
+					svinterp[g] = [f(h) for h in hgtgrid[g]]
+	
+			elif ngaps ==1 and nanidx[-1]!=svinterp.size-1:
+				first=nanidx[0]
+				last=nanidx[-1]
+				if last+1 == svinterp.size:
+					x=[hgtgrid[first-1], hgtgrid[last]]
+					y=[svinterp[first-1], svinterp[last]]
+				elif first == 0:
+					x=[hgtgrid[first], hgtgrid[last+1]]
+					y=[svinterp[first], svinterp[last+1]]
+				else:
+					x=[hgtgrid[first-1], hgtgrid[last+1]]
+					y=[svinterp[first-1], svinterp[last+1]]
+				f=interp1d(x,y)
+				svinterp[nanidx] = [f(h) for h in hgtgrid[nanidx]]
+			var.append(svinterp)
+		else:
+			''' add column to list '''
+			var.append(svinterp)
+
+	''' time grid with 20 minute spacing'''
+	t1=timestamps[0]-np.timedelta64(20,'m')
+	t2=timestamps[-1]+np.timedelta64(20,'m')
+	dates=np.arange(t1, t2, dtype='datetime64[20m]')
 
 	''' get index of each sounding date '''
 	tidx = []
@@ -147,6 +259,9 @@ def get_raw_array(soundvar,file_sound):
 
 
 def get_interp_array(soundvar,**kwargs):
+
+	''' interpolate gaps in time 
+	'''
 
 	for key,value in kwargs.iteritems():
 		if key == 'case':
@@ -180,6 +295,7 @@ def get_interp_array(soundvar,**kwargs):
 			zi = rbf(xi,hgtgrid)
 			soundarray2[:,d] = zi	
 
+	''' fill remaining gaps '''
 	if np.any(nans):
 		soundarray2 = fill_nan_gaps(soundarray2)
 
@@ -187,13 +303,19 @@ def get_interp_array(soundvar,**kwargs):
 
 def fill_nan_gaps(array):
 	
-	''' fills nans by averaging closest neighbors'''
 
+	""" search nearest neighbors """
+	a,b = array.shape
+	coords=list(product(range(a),range(b)))
+	tree = cKDTree(coords)
+	neigh = 9
+
+	''' fills nans by averaging closest neighbors'''
 	gx,gy=np.where(np.isnan(array))
+	array_col=np.reshape(array,(a*b,1))
 	for i,j in zip(gx,gy):
-		precolumn= array[i,j-1]
-		poscolumn = array[i,j+1]
-		new_value = (precolumn+poscolumn)/2
+		dist, idx = tree.query([i,j], k=neigh)
+		new_value = np.nanmean(array_col[idx])
 		array[i,j] = new_value
 	return array
 
