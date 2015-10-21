1e7201bd655d7f6f96999ce8e1cb583e11f75624
rvalenzuelar <raul.valenzuela@colorado.edu>
2015-10-20 18:23:55 -0600

    added statistical analysis

--- plotSoundTH.py
+++ plotSoundTH.py
@@ -12,6 +12,7 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import matplotlib.mlab as mlab
 
 import numpy as np
 import os 
@@ -22,16 +23,15 @@ import Thermodyn as thermo
 import Meteoframes as mf
 
 from scipy.interpolate import interp1d
-
-from  scipy.interpolate import Rbf
-
+from scipy.interpolate import Rbf
 from scipy.ndimage.filters import gaussian_filter
-
 from scipy.spatial import cKDTree
+
 from itertools import product
 
-# import seaborn as sns
 
+
+# import seaborn as sns
 # ''' set color codes in seaborn '''
 # sns.set_color_codes()
 # rc = {'axes.titlesize': 24,
@@ -40,12 +40,14 @@ from itertools import product
 # 		'xtick.labelsize':16}
 # sns.set(rc=rc)
 
+local_directory = '/Users/raulv/Documents/'
+# local_directory = 'home/rvalenzuela/'
+bvf_clevels=np.arange(-4e-4,10e-4,2e-4)
 
 
 def main():
 	
 	file_sound = get_sounding_files(None)
-	bvf_clevels=np.arange(-4e-4,10e-4,2e-4)
 
 	''' raw soundings vertically-interpolated '''
 	# # soundarray,_,_,_,_ = get_raw_array('thetaeq', file_sound)	
@@ -63,75 +65,156 @@ def main():
 	# soundarray2,_,_ = get_interp_array('v',files=file_sound)	
 	# soundarray2,_,_ = get_interp_array('DD',files=file_sound)	
 	# soundarray2,_,_ = get_interp_array('thetaeq',files=file_sound)	
-	soundarray2,hgt,_ = get_interp_array('bvf_moist',files=file_sound)	
+	soundarray2,hgt,timestamp,raw_dates = get_interp_array('bvf_moist',files=file_sound)	
 
-	''' image plot '''
-	# fig,ax=plt.subplots()
-	# im=ax.imshow(soundarray2,interpolation='none',origin='top',aspect='auto',vmin=-8e-4,vmax=8e-4)
-	# ax.invert_xaxis()
-	# plt.subplots_adjust(left=0.125,right=0.9)	
-	# plt.colorbar(im)
-	# plt.draw()	
+	# make_imshow(soundarray2,'')
 
-	''' contourf plot '''
-	rows,cols = soundarray2.shape
-	X,Y = np.meshgrid(range(cols),range(rows))
-	fig,ax=plt.subplots()
-	cf=ax.contourf(X,Y,soundarray2, levels=bvf_clevels)
-	# cf=ax.contourf(X,Y,soundarray2,levels=range(290,309))
-	plt.colorbar(cf)
-	ax.invert_xaxis()
-	plt.draw()	
+	# make_contourf(soundarray2,'unfiltered array')
 
-	''' smooth field
-	   sigma=3 seems good for BVF
-	 '''
+	'''smooth field, sigma=3 seems good for BVF'''
 	sigma=2
 	sound_filtered = gaussian_filter(soundarray2, sigma,mode='nearest')
 
-	''' filtered image plot '''
-	# fig,ax=plt.subplots()
-	# # im=ax.imshow(sound_filtered,interpolation='none',origin='top',aspect='auto')
-	# im=ax.imshow(sound_filtered[10:,:],interpolation='none',origin='top',aspect='auto',vmin=-8e-4,vmax=8e-4)
-	# ax.invert_xaxis()
-	# plt.subplots_adjust(left=0.125,right=0.9)	
-	# plt.suptitle('smoothed with gaussian filter')
-	# plt.colorbar(im)	
-	# plt.draw()	
 
-	''' filtered contourf plot '''
-	rows,cols = soundarray2.shape
-	X,Y = np.meshgrid(range(cols),range(rows))
-	fig,ax=plt.subplots()
-	cf=ax.contourf(X,Y,sound_filtered,levels=bvf_clevels)
-	# cf=ax.contourf(X,Y,sound_filtered,levels=range(290,309))
-	plt.colorbar(cf)
-	ax.invert_xaxis()
-	plt.suptitle('smoothed with gaussian filter')
-	plt.draw()	
+	x=timestamp
+	y=hgt
+	array=sound_filtered
 
-	print np.sum(sound_filtered[10:60,:])
-	print hgt[10:60]
+	make_imshow(array,'BVFm with gaussian filter',x,y,10,4000,raw_dates)
+	# make_imshow(array,'BVFm with gaussian filter',x,y,200,1200)
+	# make_imshow(array,'BVFm with gaussian filter',x,y,1200,4000)
 
-	fig,ax=plt.subplots()
-	# im=ax.imshow(sound_filtered,interpolation='none',origin='top',aspect='auto')
-	im=ax.imshow(sound_filtered[0:60,:],interpolation='none',origin='top',aspect='auto',vmin=-8e-4,vmax=8e-4)
-	ax.invert_xaxis()
-	plt.subplots_adjust(left=0.125,right=0.9)	
-	plt.suptitle('smoothed with gaussian filter')
-	plt.colorbar(im)	
-	plt.draw()	
+	# make_contourf(array,'BVFm with gaussian filter',x,y,10,4000)
+	# make_contourf(array,'BVFm with gaussian filter',x,y,200,1200)
+	# make_contourf(array,'BVFm with gaussian filter',x,y,1200,4000)
 
+	# array = make_statistical(sound_filtered,hgt,10,4000)
+	# array = make_statistical(sound_filtered,hgt,200,1200)
+	# array = make_statistical(sound_filtered,hgt,1200,4000)
 
 	# plt.show()
 	plt.show(block=False)
 
+	# return array
+
+
+
+
+def make_statistical(sound_filtered,hgt,bot,top):
+
+	bot = np.where(hgt==bot)[0]
+	top = np.where(hgt==top)[0]
+
+	''' statistical analysis of bvf '''
+	bvf_sector=sound_filtered[bot:top,:]*1e4
+	bvf_sector_rsh=np.reshape(bvf_sector,bvf_sector.size,1)
+
+	vmin=-2.
+	vmax=2.
+	vdel=0.5
+	n, bins = np.histogram(bvf_sector_rsh, bins=np.arange(vmin,vmax+vdel,vdel),normed=True)
+	n =np.append(n,0.)
+	prob = n*vdel
+	print np.sum(prob)
+	fig,ax=plt.subplots()
+	ax.bar(bins,prob,width=0.5)
+
+	mu = np.mean(bvf_sector_rsh)
+	sigma = np.std(bvf_sector_rsh)
+	res=vdel*0.1
+	x = np.arange(-5.,5.,res)
+	y = mlab.normpdf(x,mu,sigma)
+	print np.sum(y)*res
+	l = ax.plot(x, y, 'r--', linewidth=3)
+	ax.set_xlim([-2.5,2.5])
+	ax.set_ylim([0.,1.2])
+	strfloat='{:2.1f}'
+	strint='{:d}'
+	ax.text(-2,1,r'$\mu='+strfloat.format(mu)+',\ \sigma='+'{:2.1f}'.format(sigma)+'$',size=18)
+	plt.xlabel(r'$BVFm [\times10^{-4} s^{-2}]$')
+	plt.ylabel('Normal PDF')
+	bl=hgt[bot][0]
+	tl=hgt[top][0]
+	if bl==10: bl=0
+	plt.title(r'$\mathrm{Histogram\ of\ BVFm}\ Layer: '+strint.format(bl)+'-'+strint.format(tl)+'mAGL$')	
+	plt.draw()
+
+	return bvf_sector_rsh
+
+def make_contourf(array,title,X,Y,bot,top):
+
+	''' filtered contourf plot '''
+	rows,cols = array.shape
+	xx,yy = np.meshgrid(range(cols),range(rows))
+	fig,ax=plt.subplots()
+	cf=ax.contourf(xx,yy,array,levels=bvf_clevels)
+	# cf=ax.contourf(X,Y,sound_filtered,levels=range(290,309))
+	
+	xticks=range(0,X.size,3)
+	ax.set_xticks(xticks)
+	date_fmt='%d\n%H'
+	t = [pd.to_datetime(x - np.timedelta64(7, 'h')) for x in X]
+	xticklabs = [t[i].strftime(date_fmt) for i in xticks]
+	ax.set_xticklabels(xticklabs)	
+	
+	yticks = range(0,Y.size,20)
+	ax.set_yticks(yticks)
+	yticklabs =[str(Y[j]-10) for j in yticks]
+	ax.set_yticklabels(yticklabs)
+
+	bot = np.where(Y==bot)[0]
+	top = np.where(Y==top)[0]
+	ax.set_ylim([bot,top])
+
+	plt.colorbar(cf)
+	ax.invert_xaxis()
+	plt.suptitle(title)
+	plt.draw()	
+
+def make_imshow(array,title,X,Y,bot,top,raw_dates):
+
+	fig,ax=plt.subplots()
+
+	im=ax.imshow(array,interpolation='none',origin='top',aspect='auto',vmin=-8e-4,vmax=8e-4,
+						extent=[0, X.size, 1, 399])
+
+
+	xticks=range(0,X.size,3)
+	ax.set_xticks(xticks)
+	date_fmt='%d\n%H'
+	t = [pd.to_datetime(x - np.timedelta64(7, 'h')) for x in X]
+	xticklabs = [t[i].strftime(date_fmt) for i in xticks]
+	ax.set_xticklabels(xticklabs)	
+	
+	yticks = range(0,Y.size,20)
+	ax.set_yticks(yticks)
+	yticklabs =[str(Y[j]-10) for j in yticks]
+	ax.set_yticklabels(yticklabs)
+
+	bot = np.where(Y==bot)[0]
+	top = np.where(Y==top)[0]
+	ax.set_ylim([bot,top])
+
+	xidx = [np.where(X==r)[0][0] for r in raw_dates]
+	# print idx
+	# print X
+	# print raw_dates
+	yidx = np.repeat([20],len(xidx))
+	ax.plot(xidx,yidx,'o')
+
+
+	ax.invert_xaxis()
+	plt.subplots_adjust(left=0.125,right=0.98,top=0.95,bottom=0.12)	
+	plt.xlabel(r'$\Leftarrow$'+' Time [UTC]')
+	plt.ylabel('Altitude AGL [m]')
+	plt.suptitle(title)
+	plt.colorbar(im)	
+	plt.draw()
 
 def get_sounding_files(usr_case):
 
 	''' set directory and input files '''
-	# base_directory='/Users/raulv/Desktop/SOUNDING'
-	base_directory='/home/rvalenzuela/BALLOON'
+	base_directory=local_directory + 'BALLOON'
 	if not usr_case:
 		print base_directory
 		usr_case = raw_input('\nIndicate case number (i.e. 1): ')
@@ -154,7 +237,7 @@ def get_raw_array(soundvar,file_sound):
 	var=[] # 2D list
 	timestamps=[]
 	top_limit=10000 #[m]
-	hgtgrid = np.asarray(range(12, 4000,20))
+	hgtgrid = np.asarray(range(10, 4010,10))
 	for f in file_sound:
 		
 		''' sounding timestamps approached to 
@@ -255,7 +338,7 @@ def get_raw_array(soundvar,file_sound):
 	for n,i in enumerate(tidx):
 		soundarray[:,i] = var[n]
 
-	return soundarray,var,tidx,hgtgrid,dates
+	return soundarray,var,tidx,hgtgrid,dates,timestamps
 
 
 def get_interp_array(soundvar,**kwargs):
@@ -269,7 +352,7 @@ def get_interp_array(soundvar,**kwargs):
 		elif key == 'files':
 			file_sound = value
 
-	soundarray,var,tidx,hgtgrid,dates = get_raw_array(soundvar,file_sound)
+	soundarray,var,tidx,hgtgrid,dates,raw_dates = get_raw_array(soundvar,file_sound)
 
 	''' create column variables to feed Rbf '''
 	x=np.repeat(tidx,len(hgtgrid),axis=0)
@@ -299,7 +382,7 @@ def get_interp_array(soundvar,**kwargs):
 	if np.any(nans):
 		soundarray2 = fill_nan_gaps(soundarray2)
 
-	return soundarray2,hgtgrid,dates
+	return soundarray2,hgtgrid,dates,raw_dates
 
 def fill_nan_gaps(array):
 	
@@ -338,6 +421,17 @@ def get_dates(start,end,delta):
 			foo += delta
 			dates.append( foo)
 		return dates
+
+def parse_dates(file_sound):
+
+	raw_dates=[]
+	for f in file_sound:
+		fs = os.path.basename(f)[:-4]
+		raw_dates.append(datetime.strptime(fs,'%Y%m%d_%H%M%S'))
+
+	return raw_dates
+
+
 
 ''' start '''
 if __name__ == "__main__":
